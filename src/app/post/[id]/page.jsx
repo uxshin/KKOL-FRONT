feb7dd034b91858a9d57f5f7919559f9feaee21c
sunmkim <vividxxxxx@gmail.com>
2023-02-02 20:42:20 +0900

    fix api

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,50 +1,8 @@
-// import React from "react";
-// import Image from "next/image";
-// import PostInfo from "@/components/PostInfo";
-// import LayoutBuilder from "@/components/LayoutBuilder";
-// import Footer from "@/components/Footer";
-// import "server-only";
-
-// async function getPost(id) {
-//   const url = `${process.env.BASE_URL}/posts/${id}`;
-//   const res = await fetch(url);
-
-//   if (!res.ok) {
-//     throw Error();
-//   }
-//   return res.json();
-// }
-
-// const Page = async ({ params: { id }, searchParams }) => {
-//   const post = await getPost(id);
-//   const { coverImg, layout } = post;
-//   return (
-//     <div>
-//       {/* Cover */}
-//       <Image
-//         src={coverImg.url}
-//         width="1920"
-//         height="1000"
-//         className="w-full mb-[180px]"
-//       />
-//       {/* <!-- Content > */}
-//       <div className="w-3/4 mx-auto">
-//         <PostInfo post={post} />
-//         <div className="flex flex-col w-full">
-//           {layout &&
-//             layout.map((item) => <LayoutBuilder key={item.id} layout={item} />)}
-//         </div>
-
-//         <Footer isShow={true} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Page;
-
 import React from "react";
-
+import Image from "next/image";
+import Footer from "@/components/Footer";
+import PostInfo from "@/components/PostInfo";
+import LayoutBuilder from "@/components/LayoutBuilder";
 async function getPost(id) {
   const populate =
     "populate[0]=cover&populate[1]=designTeams&populate[2]=branding&populate[3]=photograph&populate[4]=contents.blockImage";
@@ -86,4 +44,4 @@ const Post = async ({ params: { id }, searchParams }) => {
   );
 };
 
-export default Page;
+export default Post;
