cbdcd48d3d4a808a2740c872feaeed3b8c71f78e
paul <paul@ntcon.co>
2023-02-03 10:26:56 +0900

    fix

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Footer from "@/components/Footer";
 import PostInfo from "@/components/PostInfo";
